ac3cc6e709113d638dbd4f44a084b71c3ac1d8b7
Taniel7 <taniel.alumets@tktk.ee>
2024-03-19 21:15:51 +0200

    add week4 code

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Enter your PIN");
+userPIN = Console.ReadLine();
+
+while (true)
+{
+    if (userPIN == "1234") ;
+}
+else
+{
+    Console.WriteLine("Wrong PIN. Try again.");
+}
+{
+    Console.WriteLine("Welcome!");
+    break;
+}
